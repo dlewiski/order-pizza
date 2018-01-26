c8e4054769165f6279f286b3a62ad6123fe2c97e
David Lewis <davidlewisp@gmail.com>
2018-01-26 10:10:46 -0800

    Make function to provide input to pizza constructor and create prototype to add ingredients to the array within the pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,36 @@
 // Business
 
 // Pizza constructor
-function PizzaMaker(name, ingredient, size) {
+function Pizza(name, size, ingredient) {
   this.name = name
+  this.size = size
   this.ingredients = []
-  this.size = small
+}
+
+//Function to provide input to constructor
+function pizzaMaker() {
+  var name = $("#name").val();
+  var size = $("#size").val();
+  var newPizza = new Pizza(name, size);
+
+  return newPizza;
+}
+
+//Prototype to push ingredients to Pizza.ingredients array
+Pizza.prototype.addIngredient = function() {
+  var ingredient = $("#ingredient").val();
+  this.ingredients.push(ingredient);
 }
 
 
 // User
 $(document).ready(function() {
+  $("#pizzaInput").submit(function(event) {
+    event.preventDefault();
+    var pizza = pizzaMaker();
+    pizza.addIngredient();
+    console.log(pizza);
+  });
 
 
 });
